7496f53befebf083649eb443629673cfcaf359e3
Lakota Rose <itsrosebro@gmail.com>
2022-04-24 17:30:11 -0500

    Added components, and created them for each function.

---
+++ src/components/displayPost.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function DisplayPost() {
+
+
+    return (
+      <div>
+        <h1>Display Post</h1>
+      </div>
+    );
+  }
+
+
+
+  export default DisplayPost;
---
+++ src/components/post.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Posts() {
+
+
+    return (
+      <div>
+        <h1>This will be the post</h1>
+      </div>
+    );
+  }
+
+
+
+  export default Posts;
