c5529ac181aa7baa14975f9a8a21f414eccc1733
Lakota Rose <itsrosebro@gmail.com>
2022-04-24 19:07:02 -0500

    Uploading files from this weekends work. Had issues with internet connection. Uploading files from the last few days. Project works as expected.

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import NavBar from './Components/NavBar/NavBar';
+import CreatePost from './Components/CreatePost/CreatePost';
+import DisplayPosts from './Components/DisplayPosts/DisplayPosts';
+import './app.css'
+
+
+function App() {
+
+  const [posts, setPosts] = useState([
+    {
+      name: 'DuckMan', 
+      post: 'Lets Hunt',
+      like: false,
+      dislike: false,
+      date: '4-22-2022'
+    },
+    {
+      name: 'Administrator', 
+      post: 'Welcome to Hunt Social Feed!',
+      like: false,
+      dislike: false,
+      date: '4-22-2022'
+    }, 
+  ])
+
+  const current = new Date();
+  const date = `${current.getMonth()+1}-${current.getDate()}-${current.getFullYear()}`
+
+  const addNewPost=(post)=> {
+    post.date = date;
+    let tempPosts = [...posts, post];
+    setPosts(tempPosts);
+  }
+
+
+  return (
+    <div className='all-container'>
+      <NavBar />
+      <div className='content-container'>
+        <CreatePost addNewPost={addNewPost} date={date}/>
+        <div className="post-container">
+          <DisplayPosts posts={posts} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './../../app.css';
+
+const NavBar = (props) => {
+    return ( 
+        <div className='navbar'>
+            <h3>Social 
+            <small>Feed</small></h3>
+        </div>
+     );
+}
+ 
+export default NavBar;
